9d35b78f9f4d320bab98564132113f84edba77d1
Alfred M <alfred.mkg@proton.me>
2023-09-15 21:41:52 +0300

    Test sort method

--- main.rb
+++ main.rb
@@ -1,7 +1,7 @@
 require_relative 'my_list'
 
 # Create our list
-list = MyList.new(1, 2, 3, 4)
+list = MyList.new(1, 4, 3, 2)
 # <MyList: @list=[1, 2, 3, 4]>
 
 # Test #all?
@@ -17,11 +17,16 @@ puts(list.any? { |e| e == 5 })
 # => false
 
 # Test #filter
+puts 'Filtered Array'
 puts(list.filter(&:even?))
 # => [2, 4]
 
-puts(list.max)
+puts "Maimum value #{list.max}"
 # => 4
 
-puts(list.min)
+puts "Minimum value #{list.min}"
 # => 1
+
+puts 'Sorted Array'
+puts(list.sort)
+# => [1, 2, 3, 4]
